5afce00d4d19f67c8f5258163608e55f698cb857
Sahil Maheshwari <sahilm1711@gmail.com>
2023-02-27 14:06:04 +0530

    Adding the change button on blockchain text

--- components/dropdown/recently_added_dropdown.jsx
+++ components/dropdown/recently_added_dropdown.jsx
@@ -25,15 +25,19 @@ const Recently_added_dropdown = ({ data, dropdownFor ,setSelectedChain}) => {
 	const [categoryDropdown, setCategoryDropdown] = useState(false);
 	const [dropdownSale, setDropdownSale] = useState(false);
 	const [currencyDropdown, setCurrencyDropdown] = useState(false);
+	const [blockChainText,setBlockChainText]=useState('Fantom')
 
 	useEffect(()=>{
 		if(setSelectedChain){
 			if(sortActive==1){
-				setSelectedChain(String(ChainId.Polygon))
+				setSelectedChain(String(ChainId.Fantom))
+				setBlockChainText('Fantom')
 			}else if(sortActive==2){
 				setSelectedChain(String(ChainId.Avalanche))
-			}else{
-				setSelectedChain(String(ChainId.Fantom))
+				setBlockChainText('Avax')
+			}else if (sortActive==3){
+				setSelectedChain(String(ChainId.Polygon))
+				setBlockChainText('Polygon')
 			}
 		}
 	},[sortActive])
@@ -270,7 +274,7 @@ const Recently_added_dropdown = ({ data, dropdownFor ,setSelectedChain}) => {
 						<path fill="none" d="M0 0h24v24H0z"></path>
 						<path d="M20 16h2v6h-6v-2H8v2H2v-6h2V8H2V2h6v2h8V2h6v6h-2v8zm-2 0V8h-2V6H8v2H6v8h2v2h8v-2h2zM4 4v2h2V4H4zm0 14v2h2v-2H4zM18 4v2h2V4h-2zm0 14v2h2v-2h-2z"></path>
 					</svg>
-					<span>Blockchain</span>
+					<span>{blockChainText}</span>
 				</button>
 
 				<div
