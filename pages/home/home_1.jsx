907b58aa858f7167383a4e6c621b67b9fc4a32b4
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-03-16 07:47:05 -0400

    Update home_1.jsx

--- pages/home/home_1.jsx
+++ pages/home/home_1.jsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {
 	Hero,
-	Hero_3,
-	Hero_10,
 	NewseLatter,
 	Browse_category,
 } from '../../components/component';
@@ -13,7 +11,6 @@ const Home_1 = () => {
 		<main>
 			<Meta title="Home 1 || Sycotic Society | NFT Marketplace Next.js Template" />
 			<Hero />
-		        <Hero_10 />
 			<Browse_category/>
 			<NewseLatter />
 		</main>
