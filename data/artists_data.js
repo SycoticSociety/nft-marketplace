f84df75966e7798556c8259fe77200d648b9c766
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-04-10 18:48:33 -0400

    Update artists_data.js

--- data/artists_data.js
+++ data/artists_data.js
@@ -62,4 +62,69 @@ export const artistInfo = [
         ],
         marketPlaceLink:'0x7979E0B2442451a2493CFC2e4264B9BeA8C86804'
 	},
+
+	{
+		title: 'General Partner at Entrepreneur',
+		desc: "Sycotic PUNKZ: Our first Polygon NFT collection.",
+		id: 'Dabbin',
+		name: 'Katie Smith',
+		img: '/images/dao/Mohawk.jpg',
+        SocialIcons:[
+            {
+              id: 1,
+              href: "https://twitter.com/SycoticSociety",
+              text: "twitter",
+            },
+            {
+              id: 2,
+              href: "https://discord.com/invite/HZH35GhyRV",
+              text: "discord",
+            },
+            {
+              id: 3,
+              href: "https://www.instagram.com/sycoticsociety/?hl=en",
+              text: "instagram",
+            }
+          ],
+        galleryImages:[
+            {
+                id: '0Amazing NFT art',
+                img: '/images/products/item_14_square.jpg',
+                title: 'Amazing NFT art',
+                authorName: 'by NFT stars',
+            },
+            {
+                id: '1Amazing NFT art',
+                img: '/images/products/item_27_square.jpg',
+                title: 'Amazing NFT art',
+                authorName: 'by Lazy Panda',
+            },
+            {
+                id: '2Punkie Cat',
+                img: '/images/products/item_28_square.jpg',
+                title: 'Punkie Cat',
+                authorName: 'by NFT stars',
+            },
+            {
+                id: '3VR Space_283',
+                img: '/images/products/item_29_square.jpg',
+                title: 'VR Space_283',
+                authorName: 'by Origin Morish',
+            },
+            {
+                id: '4TSARÉVNA',
+                img: '/images/products/item_20_square.jpg',
+                title: 'TSARÉVNA',
+                authorName: 'CryptoGuysNFT',
+            },
+            {
+                id: '5Metasmorf',
+                img: '/images/products/item_30_square.jpg',
+                title: 'Metasmorf',
+                authorName: 'by Lazy Panda',
+            },
+        ],
+        marketPlaceLink:'0x9f927cB9299B047d1e52776bAe2803fccfC0D540'
+	},
+
 ];
