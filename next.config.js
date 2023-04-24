2d5728c9ed813d1ee2e7870958e1ba962928dd97
Sahil Maheshwari <sahilm1711@gmail.com>
2023-04-24 19:18:36 +0530

    Adding these changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
-	productionBrowserSourceMaps: true,
 	webpack: (config) => {
 		config.experiments = {
 		  topLevelAwait: true,
