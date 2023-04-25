501ffc1a32f19ce9cf771fec787a99a0841441d3
Sahil Maheshwari <sahilm1711@gmail.com>
2023-04-25 11:55:29 +0530

    Removing webpack utility

--- next.config.js
+++ next.config.js
@@ -1,19 +1,4 @@
 const nextConfig = {
-	experimental: {
-		appDir: true,
-		layers:true
-	  },
-	webpack: (config) => {
-		config.experiments = {
-		  layers:true,
-		  topLevelAwait: true,
-		}
-		return config
-	  },
-	  future: {
-		webpack5: true,
-	  },
-	
 };
 
 module.exports = nextConfig;
