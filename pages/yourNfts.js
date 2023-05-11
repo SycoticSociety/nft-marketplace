290e843aff10f308e85b56136c8ba3ccd2105fbb
Sahil Maheshwari <sahilm1711@gmail.com>
2023-05-11 21:08:27 +0530

    Update yourNfts.js

--- pages/yourNfts.js
+++ pages/yourNfts.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Auctions_categories } from '../components/component'
 import { useOwnedNFTs, useContract,useAddress} from "@thirdweb-dev/react";
 
-function yourNfts() {
+function YourNfts() {
  const address = useAddress();
  const { contract, isLoading } = useContract("0xf59d868542F170DD9cDbc3D267dABB3D4A80a991");
  const { data: ownedNFTs, loadingNfts, error } = useOwnedNFTs(contract, address);
@@ -15,4 +15,4 @@ function yourNfts() {
   )
 }
 
-export default yourNfts
+export default YourNfts
