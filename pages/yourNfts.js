593b1fef26aa04b00a4a4adfd6c9c6aab7964fd6
Sahil Maheshwari <sahilm1711@gmail.com>
2023-05-13 00:08:02 +0530

    Adding nft changes

--- pages/yourNfts.js
+++ pages/yourNfts.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { Auctions_categories } from '../components/component'
 import { useOwnedNFTs, useContract,useAddress,ConnectWallet} from "@thirdweb-dev/react";
-import { useMetaMask } from 'metamask-react';
+import { useMetamask } from 'metamask-react';
 
 function YourNfts() {
  const address = useAddress();
- const connectWithMetaMask=useMetaMask()
+ const connectWithMetamask = useMetamask();
  const { contract, isLoading } = useContract("0xf59d868542F170DD9cDbc3D267dABB3D4A80a991");
  const { data: ownedNFTs, loadingNfts, error } = useOwnedNFTs(contract, address);
   return (
@@ -14,7 +14,7 @@ function YourNfts() {
     <>
       <h1 className='font-display text-jacarta-700 py-8 text-center text-xl font-medium dark:text-white'>Connect your wallet to see your nfts.</h1>
       <button
-        onClick={()=>connectWithMetaMask()}
+        onClick={()=>connectWithMetamask()}
         className="bg-accent cursor-default rounded-full py-3 px-8 text-center font-semibold text-white transition-all" 
       >
       Connect Your Wallet
