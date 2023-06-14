01fdacbc38ecbe19469b6aac7e6c6142d2b28746
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-06-14 18:56:31 -0400

    Update hero.jsx

--- components/hero/hero.jsx
+++ components/hero/hero.jsx
@@ -42,7 +42,7 @@ const hero = () => {
               </Link>
               <Link href=" ">
                 <a className="text-accent shadow-white-volume hover:bg-accent-dark hover:shadow-accent-volume w-fit-content rounded-full bg-green py-3 px-8 text-center font-semibold transition-all hover:text-white">
-                Tools & Utilities!
+                Tools, Utilities & More!
                 </a>
               </Link>
             </div>
