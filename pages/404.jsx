38bb653d9becf95d02e72fb074bb22ea6d113f03
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-06-14 19:44:59 -0400

    Update 404.jsx

--- pages/404.jsx
+++ pages/404.jsx
@@ -6,7 +6,7 @@ import Meta from "../components/Meta";
 const Error_page = () => {
   return (
     <div>
-      <Meta title="404 || Xhibiter | NFT Marketplace Next.js Template" />
+      <Meta title="404 || Sycotic Society| NFT Marketplace" />
       <div className="pt-[5.5rem] lg:pt-24">
         {/* <!-- 404 --> */}
         <section className="dark:bg-jacarta-800 relative py-16 md:py-24">
@@ -30,11 +30,12 @@ const Error_page = () => {
                 />
               </span>
               <h1 className="text-jacarta-700 font-display mb-6 text-4xl dark:text-white md:text-6xl">
-                Page Not Found!
+                Coming Soon!
               </h1>
               <p className="dark:text-jacarta-300 mb-12 text-lg leading-normal">
-                Oops! The page you are looking for does not exist. It might have
-                been moved or deleted.
+                Sycotic 💢 Society is working continuously around
+                the clock to bring you the most interactive useful website
+                possible.  Thank you for your patience and support!
               </p>
               <Link href="/">
                 <a className="bg-accent shadow-accent-volume hover:bg-accent-dark inline-block rounded-full py-3 px-8 text-center font-semibold text-white transition-all">
