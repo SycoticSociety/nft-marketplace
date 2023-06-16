dd29f7cbb9e2ff00c2cb516f54bbb829ad15f9f2
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-06-15 23:40:55 -0400

    Update TrustedPartner.jsx

--- components/dao/TrustedPartner.jsx
+++ components/dao/TrustedPartner.jsx
@@ -14,10 +14,9 @@ const TrustedPartner = () => {
         <div className="container">
           <div className="mx-auto mb-14 max-w-xl text-center">
             <h2 className="mb-6 text-center font-display text-3xl font-medium text-jacarta-700 dark:text-white">
-              
+              Sycotic 💢 Society Trusted Partners.
             </h2>
             <p className="text-lg dark:text-jacarta-300">
-            Sycotic 💢 Society:  We are in good company!
             </p>
           </div>
         </div>
