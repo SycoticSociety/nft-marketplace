ad1e15bcff8ede7d692d7eab7d0d35055bb44ff5
Sahil Maheshwari <sahilm1711@gmail.com>
2023-06-18 16:47:07 +0530

    Making partner 2 reponsive

--- components/dao/Partner2.jsx
+++ components/dao/Partner2.jsx
@@ -1,29 +1,25 @@
 const Partner2 = () => {
   const partnerItem = [
-    "partner_dao_8",
-    "partner_dao_9",
-    "partner_dao_10",
-    "partner_dao_11",
-    "partner_dao_12",
-    "partner_dao_13",
-    "partner_dao_14",
-    "partner_dao_8",
-    "partner_dao_9",
-    "partner_dao_10",
-    "partner_dao_11",
-    "partner_dao_12",
-    "partner_dao_13",
-    "partner_dao_14",
+    {link:'https://www.cameltoecan.com/',image:'partner_dao_1'},
+    {link:'https://swap.crodex.app/#/swap',image:'partner_dao_2'},
+    {link:'https://phenix.finance/',image:'partner_dao_8'},
+    {link:'https://www.cameltoecan.com/',image:'partner_dao_10'},
+    {link:'https://swap.crodex.app/#/swap',image:'partner_dao_10'},
+    {link:'https://phenix.finance/',image:'partner_dao_10'},
+    {link:'https://www.cameltoecan.com/',image:'partner_dao_2'},
+    {link:'https://swap.crodex.app/#/swap',image:'partner_dao_12'},
+    {link:'https://phenix.finance/',image:'partner_dao_13'},
   ];
   return (
     <>
       {partnerItem.map((item, i) => (
-        <div
+        <a
+          href={item.link}
           className="flex flex-shrink-0 items-center justify-center rounded-2.5xl bg-jacarta-900 opacity-80 p-4 p-6"
           key={i}
         >
-          <img src={`/images/dao/${item}.jpg`} alt="partner item" />
-        </div>
+          <img src={`/images/dao/${item.image}.jpg`} alt="partner item" />
+        </a>
       ))}
     </>
   );
