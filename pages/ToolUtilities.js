ae0de42f5f7df9bfcafba0c0efd67ba20fb9f05b
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-06-21 07:31:30 -0400

    Update ToolUtilities.js

--- pages/ToolUtilities.js
+++ pages/ToolUtilities.js
@@ -1,17 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import UserContext from "../components/UserContext";
-
-import Partners2 from "../../components/partners/Partners2";
-import CryptoCounter from "../../components/cryto-trading/CryptoCounter";
-import CryptoPrice from "../../components/cryto-trading/price";
-import Features from "../../components/cryto-trading/Features";
-import InvestEarn from "../../components/cryto-trading/invest-earn";
-import ProcessCta from "../../components/cryto-trading/ProcessCta";
-import Hero_11 from "../../components/hero/hero_11";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import Meta from "../components/Meta";
+import Home_4 from './home/home_4'
 
 export default function Home() {
   const { scrollRef } = useContext(UserContext);
@@ -28,11 +17,8 @@ export default function Home() {
   });
 
   return (
-      <Meta title="| Sycotic Society | NFT Marketplace" />
-      <main>
-      <Hero_11 />
-      <CryptoPrice />
-      <InvestEarn />
-      <main/>
+    <>
+      <Home_4/>
+    </>
   );
 }
