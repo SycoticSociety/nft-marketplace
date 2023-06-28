6087ce01b5a8748d3922d0c881c1bcdb57d25ba4
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-06-28 15:50:08 -0400

    Update hero_5.jsx

--- components/hero/hero_5.jsx
+++ components/hero/hero_5.jsx
@@ -38,18 +38,7 @@ const Hero_5 = () => {
 										
 									</span>
 								</div>
-								<div className="mb-4 flex-1 rounded-2lg bg-white p-4 text-center dark:bg-white/[.15]">
-									<span className="block font-display text-3xl text-[#737EF2]">Token Airdropper</span>
-									<span className="block font-display text-sm text-jacarta-500 dark:text-white">
-										
-									</span>
-								</div>
-								<div className="mb-4 flex-1 rounded-2lg bg-white p-4 text-center dark:bg-white/[.15]">
-									<span className="block font-display text-3xl text-[#F35BC7]">Darkest Hour Mint</span>
-									<span className="block font-display text-sm text-jacarta-500 dark:text-white">
-										
-									</span>
-								</div>
+								
 								
 							</div>
 							<h1 className="mb-6 text-center font-display text-5xl text-jacarta-700 dark:text-white md:text-left lg:text-5xl xl:text-6xl">
