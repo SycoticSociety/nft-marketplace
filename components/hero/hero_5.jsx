cc6f6a9fb4cb21efd01c16d27b8b4616fe8b0aa2
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-06-28 13:39:30 -0400

    Update hero_5.jsx

--- components/hero/hero_5.jsx
+++ components/hero/hero_5.jsx
@@ -19,7 +19,7 @@ const Hero_5 = () => {
 						<div className="col-span-6 flex h-full flex-col items-center justify-center py-10 md:items-start md:py-20 xl:col-span-5 xl:pl-[20%] xl:pr-[10%]">
 							<div className="mb-10 w-full sm:flex sm:space-x-4">
 								<div className="mb-4 flex-1 rounded-2lg bg-white p-4 text-center dark:bg-white/[.15]">
-									<span className="block font-display text-3xl text-[#8DD059]">Minting Now!</span>
+									<span className="block font-display text-3xl text-[#8DD059]">Token Balance</span>
 									<span className="block font-display text-sm text-jacarta-500 dark:text-white">
 										
 									</span>
@@ -36,6 +36,26 @@ const Hero_5 = () => {
 										
 									</span>
 								</div>
+
+                                                                <div className="mb-4 flex-1 rounded-2lg bg-white p-4 text-center dark:bg-white/[.15]">
+									<span className="block font-display text-3xl text-[#8DD059]">Virtual Wallet</span>
+									<span className="block font-display text-sm text-jacarta-500 dark:text-white">
+										
+									</span>
+								</div>
+								<div className="mb-4 flex-1 rounded-2lg bg-white p-4 text-center dark:bg-white/[.15]">
+									<span className="block font-display text-3xl text-[#737EF2]">Token Airdropper</span>
+									<span className="block font-display text-sm text-jacarta-500 dark:text-white">
+										
+									</span>
+								</div>
+								<div className="mb-4 flex-1 rounded-2lg bg-white p-4 text-center dark:bg-white/[.15]">
+									<span className="block font-display text-3xl text-[#F35BC7]">Darkest Hour Mint</span>
+									<span className="block font-display text-sm text-jacarta-500 dark:text-white">
+										
+									</span>
+								</div>
+								
 							</div>
 							<h1 className="mb-6 text-center font-display text-5xl text-jacarta-700 dark:text-white md:text-left lg:text-5xl xl:text-6xl">
 							
