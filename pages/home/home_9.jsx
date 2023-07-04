e962d01f3022fb80b410a7cb35137b6dc4b04fc1
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-07-04 13:50:23 -0400

    Update home_9.jsx

--- pages/home/home_9.jsx
+++ pages/home/home_9.jsx
@@ -6,6 +6,7 @@ import NewseLatter2 from "../../components/dao/newseLatter2";
 import { Partners } from "../../components/component";
 import Hero_9 from "../../components/hero/hero_9";
 import Meta from "../../components/Meta";
+import Browse_category from '../../components/component';
 import CryptoPrice from "../../components/cryto-trading/price";
 import Hero_11 from "../../components/hero/hero_11";
 import TrustedPartner from "../../components/dao/TrustedPartner";
@@ -20,8 +21,9 @@ const Home_9 = () => {
     <>
       <Meta title="Home 9 || Xhibiter | NFT Marketplace Next.js Template" />
       <Hero_9 />
-    
+      
       <CryptoPrice />
+      <Browse_category />
   
   
     </>
