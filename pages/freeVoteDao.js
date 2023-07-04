df37f3a78b89211b42499079af3150ab221fbcab
Sahil Maheshwari <sahilm1711@gmail.com>
2023-07-04 21:15:39 +0530

    Update freeVoteDao.js

--- pages/freeVoteDao.js
+++ pages/freeVoteDao.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react";
-import Benifits from "../../components/dao/Benifits";
-import Intro from "../../components/dao/Intro";
-import Participate from "../../components/dao/Participate";
-import NewseLatter2 from "../../components/dao/newseLatter2";
-import { Partners } from "../../components/component";
-import Hero_9 from "../../components/hero/hero_9";
-import Meta from "../../components/Meta";
-import TrustedPartner from "../../components/dao/TrustedPartner";
+import Benifits from "../components/dao/Benifits";
+import Intro from "../components/dao/Intro";
+import Participate from "../components/dao/Participate";
+
+import Hero_9 from "../components/hero/hero_9";
+import Meta from "../components/Meta";
+import TrustedPartner from "../components/dao/TrustedPartner";
 
 const Home_9 = () => {
   useEffect(() => {
