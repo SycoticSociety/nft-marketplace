71ddf81807399d1e6468bccf89258cf9dcaef816
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-07-24 18:51:27 -0400

    Update hero_11.jsx

--- components/hero/hero_11.jsx
+++ components/hero/hero_11.jsx
@@ -80,6 +80,14 @@ const hero_11 = () => {
           </svg>
         </div>
         {/* End absolute bottom */}
+         <iframe
+    src="https://bafybeic54udbggbhtn7nfjrrj7elrim4shfped7c3cco7neset7fdwpwmu.gateway.ipfscdn.io/erc1155.html?contract=0x65D8A98596D25Df1700F6258183791E070ebD195&chain=%7B%22name%22%3A%22Cronos+Mainnet+Beta%22%2C%22chain%22%3A%22CRO%22%2C%22rpc%22%3A%5B%22https%3A%2F%2Fcronos-beta.rpc.thirdweb.com%2Fa8a7f8aa7ddd8115bad1892b179be99f%22%5D%2C%22nativeCurrency%22%3A%7B%22name%22%3A%22Cronos%22%2C%22symbol%22%3A%22CRO%22%2C%22decimals%22%3A18%7D%2C%22shortName%22%3A%22cro%22%2C%22chainId%22%3A25%2C%22testnet%22%3Afalse%2C%22slug%22%3A%22cronos-beta%22%7D&tokenId=0&theme=dark&primaryColor=cyan"
+    width="600px"
+    height="600px"
+    style="max-width:100%;"
+    frameborder="0"
+    ></iframe>
+        
       </section>
       {/* <!-- end hero -->  */}
     </>
