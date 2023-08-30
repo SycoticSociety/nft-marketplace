c50fce8be3b2fdcc5ccc7ca282bf81e6581be70a
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-08-30 13:36:03 -0400

    Update EmbeddedPage.js

--- components/EmbeddedPage.js
+++ components/EmbeddedPage.js
@@ -1,13 +1,23 @@
 import React from 'react';
+import imagePlaceholder from './image-placeholder.png'; // Replace with your image URL
+import './EmbeddedPage.css'; // You can define your CSS classes here
 
 const EmbeddedPage = () => {
+  const openExternalSite = () => {
+    // Replace this with the URL you want to open
+    const externalUrl = 'https://www.convex.dev/ai-town';
+    
+    // Open the URL in a new window or tab
+    window.open(externalUrl, '_blank');
+  };
+
   return (
-    <div>
-      <iframe
-        src="https://www.convex.dev/ai-town"
-        width="100%"
-        height="600px"
-        title="Embedded AI Town"
+    <div className="embedded-page-container">
+      <img
+        src={imagePlaceholder}
+        alt="Embedded AI Town"
+        className="embedded-image"
+        onClick={openExternalSite}
       />
     </div>
   );
