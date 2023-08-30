f43bafb7ebce85f35d813b9cb40e7c54965cf9f3
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-08-30 12:47:37 -0400

    Create EmbeddedPage.js

---
+++ components/EmbeddedPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const EmbeddedPage = () => {
+  return (
+    <div>
+      <iframe
+        src="https://www.convex.dev/ai-town"
+        width="100%"
+        height="600px"
+        title="Embedded AI Town"
+      />
+    </div>
+  );
+};
+
+export default EmbeddedPage;
