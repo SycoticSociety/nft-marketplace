92e0b5aca8f4dc51daef48b8ab23b6954fecf302
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-09-06 01:13:22 -0400

    Update statistic_promo_2.jsx.

--- components/promo/statistic_promo_2.jsx
+++ components/promo/statistic_promo_2.jsx
@@ -12,7 +12,7 @@ const Statistic_promo_2 = () => {
 					<div className="items-center justify-between lg:flex">
 						<div className="mb-12 lg:w-[45%] lg:pr-16">
 							<h2 className="mb-6 font-display text-2xl text-white">
-								Sycotic Society. Utilizing extremely powerful A.I components to accelerate our development process.
+								Sycotic Society and Artificial Intelligence.  A match made in heaven.
 							</h2>
 							<p className="mb-8 text-lg leading-normal text-jacarta-200">
 						          Although we are currently in the early phases of development, we have achieved many great feats to date. Looking forward
