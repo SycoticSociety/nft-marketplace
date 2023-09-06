c1a0df1d9e031c5323f429df986281669a6551c4
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-09-05 22:11:51 -0400

    Update statistic_promo_2.jsx

--- components/promo/statistic_promo_2.jsx
+++ components/promo/statistic_promo_2.jsx
@@ -12,16 +12,13 @@ const Statistic_promo_2 = () => {
 					<div className="items-center justify-between lg:flex">
 						<div className="mb-12 lg:w-[45%] lg:pr-16">
 							<h2 className="mb-6 font-display text-2xl text-white">
-								Find and Fight rare Creatures and Collect Stunning Pieces
+								Find rare Creatures and Collect Stunning Pieces.
 							</h2>
 							<p className="mb-8 text-lg leading-normal text-jacarta-200">
-								Employees are our number-one priority, so we like to take care of them!
+						
 							</p>
 							<p className="mb-12 text-jacarta-200">
-								Every digital creation available through MakersPlace is an authentic and truly
-								unique digital creation, signed and issued by the creator — made possible by
-								blockchain technology. Even if the digital creation is copied, it won't be the
-								authentic and originally signed version.
+						
 							</p>
 							<Link href="/collection/explore_collection">
 								<a className="inline-block rounded-full bg-accent py-3 px-8 text-center font-semibold text-white shadow-accent-volume transition-all hover:bg-accent-dark">
