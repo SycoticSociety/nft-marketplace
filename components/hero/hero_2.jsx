903567fddd12a100fd891a2b29c13e562e86ed34
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-09-05 20:36:44 -0400

    Update hero_2.jsx

--- components/hero/hero_2.jsx
+++ components/hero/hero_2.jsx
@@ -4,31 +4,27 @@ import Image from "next/image";
 
 const Hero_2 = () => {
   return (
-    <>
-      {/* <!-- Hero --> */}
-
-        <div className="container">
-          <div className="py-24 text-center">
-            <h1 className="text-jacarta-700 font-display mb-6 text-5xl dark:text-white lg:text-6xl xl:text-7xl">
-             
-            </h1>
-            <p className="dark:text-jacarta-200 mb-8 text-lg">
-             
-            </p>
-            <div className="inline-flex space-x-4">
-            
-            
-            </div>
-          </div>
-
-          <div className="relative">
-            {/* <!-- Slider --> */}
-            <ArtsCarousel />
-          </div>
+    /* <!-- Hero --> */
+    <div className="container">
+      <div className="py-24 text-center">
+        <h1 className="text-jacarta-700 font-display mb-6 text-5xl dark:text-white lg:text-6xl xl:text-7xl">
+         
+        </h1>
+        <p className="dark:text-jacarta-200 mb-8 text-lg">
+         
+        </p>
+        <div className="inline-flex space-x-4">
+        
+        
         </div>
-      </section>
-      {/* <!-- end hero --> */}
-    </>
+      </div>
+
+      <div className="relative">
+        {/* <!-- Slider --> */}
+        <ArtsCarousel />
+      </div>
+    </div>
+    /* <!-- end hero --> */
   );
 };
 
