246e29a0847fa7c74649849aad3a9a33bdd282e4
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-09-06 02:24:03 -0400

    Update newseLatterData.js

--- data/newseLatterData.js
+++ data/newseLatterData.js
@@ -3,14 +3,14 @@ const newseLatterData = [
 		id: '1',
 		icon: { parentBg: '#beaaf7', childBg: 'rgb(131 88 255) ', svg: 'icon-wallet' },
         link:'/yourNfts',
-		title: [' Set ', 'up ', 'your ', 'wallet '],
+		title: [' View ', 'Your ', 'NFT ', 'Collection '],
 		text: "Once you've set up your wallet of choice, connect it to Open Sea by clicking the NFT Marketplacein the topright corner.",
 	},
 	{
 		id: '2',
 		icon: { parentBg: '#c4f2e3', childBg: 'rgb(16 185 129)', svg: 'icon-wallet' },
         link:'/create',
-		title: [' Create ', ' Your ', ' Collection '],
+		title: [' Single  ', ' Mint ', ' NFTs '],
 		text: 'Click Create, and set up your collection. Add social links, a description, profile & banner images, and set a secondary sales fee.',
 	},
 	{
