d847360f4b84dc9d510fe9a5ca531d94a9c94c3a
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-09-05 20:33:24 -0400

    Update hero_2.jsx

--- components/hero/hero_2.jsx
+++ components/hero/hero_2.jsx
@@ -6,21 +6,6 @@ const Hero_2 = () => {
   return (
     <>
       {/* <!-- Hero --> */}
-      <section className="hero relative py-20 md:pt-32">
-        <picture className="pointer-events-none absolute inset-x-0 top-0 -z-10 dark:hidden">
-          <figure className="h-[630px] w-full">
-            <Image src="/images/gradient.jpg" alt="gradient" layout="fill" />
-          </figure>
-        </picture>
-        <picture className="pointer-events-none absolute inset-x-0 top-0 -z-10 hidden dark:block">
-          <figure className="h-[630px] w-full">
-            <Image
-              src="/images/gradient_dark.jpg"
-              alt="gradient dark"
-              layout="fill"
-            />
-          </figure>
-        </picture>
 
         <div className="container">
           <div className="py-24 text-center">
