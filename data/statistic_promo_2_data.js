1244e2ce3930951f8cb084aeb550566121f432bf
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-09-06 00:23:30 -0400

    Update statistic_promo_2_data.js

--- data/statistic_promo_2_data.js
+++ data/statistic_promo_2_data.js
@@ -2,17 +2,21 @@ export const statistic_promo_2_data = [
 	{
 		id: '1',
 		img: '/images/nft-game/robot_large_1.png',
+		href: 'https://example.com/link1', // Add the URL you want to open for this image
 	},
 	{
 		id: '2',
 		img: '/images/nft-game/robot_large_2.png',
+		href: 'https://example.com/link2', // Add the URL you want to open for this image
 	},
 	{
 		id: '3',
 		img: '/images/nft-game/robot_large_3.png',
+		href: 'https://example.com/link3', // Add the URL you want to open for this image
 	},
 	{
 		id: '4',
 		img: '/images/nft-game/robot_large_4.png',
+		href: 'https://example.com/link4', // Add the URL you want to open for this image
 	},
 ];
