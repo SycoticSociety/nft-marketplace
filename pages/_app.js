3900f600eefc8ed8881222da63265ac74781109a
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-09-13 18:29:33 -0400

    Update _app.js

---
+++ pages/_app.js
@@ -0,0 +1,69 @@
+
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import Layout from "../components/layout";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
+import { useRouter } from "next/router";
+import Meta from "../components/Meta";
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
+import UserContext from "../components/UserContext";
+import ChainContext from "../components/chainContext";
+import { useEffect, useRef, useState } from "react";
+import { MetaMaskProvider } from "metamask-react";
+
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const pid = router.asPath;
+  const scrollRef = useRef({
+    scrollPos: 0,
+  });
+  const [selectedChain, setSelectedChain] = useState(ChainId.Fantom);
+  const [selectedMarketplace, setSelectedMarketplace] = useState("0x7780Afb7243Fb6d706eBA2a99EEaF492bc94F171");
+
+  useEffect(()=>{
+    if(router.pathname){
+      if(router.pathname?.includes('fantom')){
+        setSelectedChain(ChainId.Fantom)
+      }else if(router.pathname.includes('polygon')){
+        setSelectedChain(ChainId.Polygon)
+      }else if(router.pathname.includes('arbitrum')){
+        setSelectedChain(ChainId.Arbitrum)
+      }
+    }else if(router.pathname=='/create'){
+      setSelectedChain(ChainId.Polygon)
+    }else if (router.pathname=='/yourNfts'){
+      setSelectedChain(ChainId.Polygon)
+    }
+  },[
+    router.pathname
+  ])
+
+  return (
+    <>
+      <Meta title="Sycotic Society | NFT Marketplace" />
+      <Provider store={store}>
+        <ThemeProvider enableSystem={true} attribute="class">
+          <ChainContext.Provider value={{ selectedChain, setSelectedChain , selectedMarketplace, setSelectedMarketplace}}>
+              <ThirdwebProvider desiredChainId={selectedChain}>
+                <MetaMaskProvider>
+
+                  <UserContext.Provider value={{ scrollRef: scrollRef }}>
+                    {pid === "/login" ? (
+                      <Component {...pageProps} />
+                    ) : (
+                      <Layout>
+                        <Component {...pageProps} />
+                      </Layout>
+                    )}
+                  </UserContext.Provider>
+                  </MetaMaskProvider>
+              </ThirdwebProvider>
+          </ChainContext.Provider>
+        </ThemeProvider>
+      </Provider>
+    </>
+  );
+}
+
+export default MyApp;
