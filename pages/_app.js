908f651a9a4ff9ed309cdc87ad7539abbaf4e50e
Sycotic Society <thecrazydutchman2000@gmail.com>
2023-09-13 19:12:31 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,18 @@
+
+import "../styles/globals.css";
+import { ThemeProvider } from "next-themes";
+import Layout from "../components/layout";
+import { Provider } from "react-redux";
+import { store } from "../redux/store";
+import { useRouter } from "next/router";
 import Meta from "../components/Meta";
-import Layout from '../../components/Layout'; // Adjust the import path accordingly
-import { ThemeProvider } from 'styled-components';
+import { ThirdwebProvider, ChainId } from "@thirdweb-dev/react";
 import UserContext from "../components/UserContext";
 import ChainContext from "../components/chainContext";
 import { useEffect, useRef, useState } from "react";
 import { MetaMaskProvider } from "metamask-react";
-import { ThirdwebProvider, ConnectWallet, metamaskWallet, ChainId } from "@thirdweb-dev/react";
-import { useRouter } from 'next/router'; // Import useRouter from next/router
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   const router = useRouter();
   const pid = router.asPath;
   const scrollRef = useRef({
@@ -17,45 +21,49 @@ export default function MyApp({ Component, pageProps }) {
   const [selectedChain, setSelectedChain] = useState(ChainId.Fantom);
   const [selectedMarketplace, setSelectedMarketplace] = useState("0x7780Afb7243Fb6d706eBA2a99EEaF492bc94F171");
 
-  useEffect(() => {
-    if (router.pathname) {
-      if (router.pathname?.includes('fantom')) {
-        setSelectedChain(ChainId.Fantom);
-      } else if (router.pathname.includes('polygon')) {
-        setSelectedChain(ChainId.Polygon);
-      } else if (router.pathname.includes('arbitrum')) {
-        setSelectedChain(ChainId.Arbitrum);
+  useEffect(()=>{
+    if(router.pathname){
+      if(router.pathname?.includes('fantom')){
+        setSelectedChain(ChainId.Fantom)
+      }else if(router.pathname.includes('polygon')){
+        setSelectedChain(ChainId.Polygon)
+      }else if(router.pathname.includes('arbitrum')){
+        setSelectedChain(ChainId.Arbitrum)
       }
-    } else if (router.pathname == '/create') {
-      setSelectedChain(ChainId.Polygon);
-    } else if (router.pathname == '/yourNfts') {
-      setSelectedChain(ChainId.Polygon);
+    }else if(router.pathname=='/create'){
+      setSelectedChain(ChainId.Polygon)
+    }else if (router.pathname=='/yourNfts'){
+      setSelectedChain(ChainId.Polygon)
     }
-  }, [router.pathname]);
+  },[
+    router.pathname
+  ])
 
   return (
     <>
       <Meta title="Sycotic Society | NFT Marketplace" />
-      {/* Remove the Provider import for clarity */}
-      {/* <Provider store={store}> */}
-      <ThemeProvider enableSystem={true} attribute="class">
-        <ChainContext.Provider value={{ selectedChain, setSelectedChain, selectedMarketplace, setSelectedMarketplace }}>
-          <ThirdwebProvider desiredChainId={selectedChain}>
-            <MetaMaskProvider>
-              <UserContext.Provider value={{ scrollRef: scrollRef }}>
-                {pid === "/login" ? (
-                  <Component {...pageProps} />
-                ) : (
-                  <Layout>
-                    <Component {...pageProps} />
-                  </Layout>
-                )}
-              </UserContext.Provider>
-            </MetaMaskProvider>
-          </ThirdwebProvider>
-        </ChainContext.Provider>
-      </ThemeProvider>
-      {/* </Provider> */}
+      <Provider store={store}>
+        <ThemeProvider enableSystem={true} attribute="class">
+          <ChainContext.Provider value={{ selectedChain, setSelectedChain , selectedMarketplace, setSelectedMarketplace}}>
+              <ThirdwebProvider desiredChainId={selectedChain}>
+                <MetaMaskProvider>
+
+                  <UserContext.Provider value={{ scrollRef: scrollRef }}>
+                    {pid === "/login" ? (
+                      <Component {...pageProps} />
+                    ) : (
+                      <Layout>
+                        <Component {...pageProps} />
+                      </Layout>
+                    )}
+                  </UserContext.Provider>
+                  </MetaMaskProvider>
+              </ThirdwebProvider>
+          </ChainContext.Provider>
+        </ThemeProvider>
+      </Provider>
     </>
   );
 }
+
+export default MyApp;
