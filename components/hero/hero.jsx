403942171bd41d20776b2774c9b65f2aad085496
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-01-11 19:52:15 -0500

    Update hero.jsx

--- components/hero/hero.jsx
+++ components/hero/hero.jsx
@@ -54,7 +54,8 @@ const hero = () => {
               </Link>
               <Link href="https://swap.crodex.app/#/swap?outputCurrency=0xe7f3c7c6611357313b5c2734341fe9cbad1d9f59">
                  <a className="bg-accent shadow-accent-volume hover:bg-accent-dark w-fit-content rounded-full py-3 px-8 text-center font-semibold text-white transition-all">
-                  Swap Sycotic💢Society.
+                  Swap Sycotic 
+                   💢.
                 </a>
               </Link>
               <Link href="https://swap.crodex.app/#/swap?outputCurrency=0x3E68eA1588E32e51D98dBda6Ea05128513ECC713">
