5bfdff6cfe4b750c1b23301067ee192f663d6f45
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-01-11 19:57:31 -0500

    Update hero.jsx

--- components/hero/hero.jsx
+++ components/hero/hero.jsx
@@ -49,7 +49,7 @@ const hero = () => {
             <div className="flex space-x-4">
               <Link href="https://swap.crodex.app/#/swap?outputCurrency=0x4E2E7700C8f8A414b4AF8828f54330a304B6625d">
                   <a className="text-white shadow-white-volume hover:bg-orange hover:shadow-accent-volume w-fit-content rounded-full bg-orange py-3 px-8 text-center font-semibold transition-all hover:text-white">
-                  Swap Free🪙Coin
+                  Swap Free Coin 🪙
                 </a>
               </Link>
               <Link href="https://swap.crodex.app/#/swap?outputCurrency=0xe7f3c7c6611357313b5c2734341fe9cbad1d9f59">
@@ -60,7 +60,7 @@ const hero = () => {
               </Link>
               <Link href="https://swap.crodex.app/#/swap?outputCurrency=0x3E68eA1588E32e51D98dBda6Ea05128513ECC713">
                 <a className="text-white shadow-white-volume hover:bg-gray hover:shadow-accent-volume w-fit-content rounded-full bg-gray py-3 px-8 text-center font-semibold transition-all hover:text-white">
-                  Swap Broke🐒ASF
+                  Swap Broke ASF 🦧
                 </a>
               </Link>
             </div>
