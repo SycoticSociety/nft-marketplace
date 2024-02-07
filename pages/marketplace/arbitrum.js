0d3dbe6e1d95b987fb78cc1dbbd466d0adea59c9
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-02-07 08:10:49 -0500

    Update arbitrum.js

--- pages/marketplace/arbitrum.js
+++ pages/marketplace/arbitrum.js
@@ -12,7 +12,7 @@ export default function Arbitrum() {
   const { selectedChain, setSelectedChain, selectedMarketplace, setSelectedMarketplace } = useContext(ChainContext);
 
   // Define your API key (replace 'YOUR_API_KEY' with your actual key)
-  const apiKey = "c6283d4d5955100fffcdd22978b8190a";
+  const apiKey = "7976449a6362f89d61ead11d0753cfd8";
 
   useEffect(() => {
     setSelectedChain(ChainId.Arbitrum);
