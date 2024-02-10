7712b6e8aaffc1b368960c7a333f9a493a365886
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-02-09 22:45:16 -0500

    Update hero.jsx

--- components/hero/hero.jsx
+++ components/hero/hero.jsx
@@ -28,7 +28,7 @@ const hero = () => {
       <div className="container h-full mx-auto">
         <div className="grid h-full items-center gap-4 md:grid-cols-12">
           <div className="col-span-6 flex h-full flex-col items-center justify-center py-10 md:items-start md:py-20 xl:col-span-4">
-            <h1 className="text-jacarta-700 font-bold font-display mb-6 text-center text-5xl dark:text-white md:text-left lg:text-6xl xl:text-7xl">
+            <h1 className="text-jacarta-700 font-bold font-display mb-6 text-center text-4xl dark:text-white md:text-left lg:text-6xl xl:text-7xl">
               Now Trade,{" "}
               <span className="animate-gradient">
                 <a href="https://swap.crodex.app/#/swap?outputCurrency=0xe7f3c7c6611357313b5c2734341fe9cbad1d9f59"> Sycotic Society, </a>
