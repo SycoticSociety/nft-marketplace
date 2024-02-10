078523d2c7cc9c84ca5909af6b3c4e2cc1950571
Sycotic Society <thecrazydutchman2000@gmail.com>
2024-02-09 23:50:01 -0500

    Update hero.jsx

--- components/hero/hero.jsx
+++ components/hero/hero.jsx
@@ -26,9 +26,9 @@ const hero = () => {
       </picture>
 
       <div className="container h-full mx-auto">
-        <div className="grid h-full items-center gap-4 md:grid-cols-12">
-          <div className="col-span-6 flex h-full flex-col items-center justify-center py-10 md:items-start md:py-20 xl:col-span-4">
-            <h1 className="text-jacarta-700 font-bold font-display mb-6 text-center text-4xl dark:text-white mb-6:text-center lg:text-5xl ">
+        <div className="grid h-full items-center justify-center gap-4 md:grid-cols-12">
+          <div className="col-span-6 flex h-full flex-col items-center justify-center py-10 xl:col-span-4">
+            <h1 className="text-jacarta-700 font-bold font-display mb-6 text-center text-4xl dark:text-white mb:text-center lg:text-5xl lg:text-5xl">
               Now Trade,{" "}
               <span className="animate-gradient">
                 <a href="https://swap.crodex.app/#/swap?outputCurrency=0xe7f3c7c6611357313b5c2734341fe9cbad1d9f59"> Sycotic Society, </a>
@@ -38,59 +38,52 @@ const hero = () => {
                 <a href="https://swap.crodex.app/#/swap?outputCurrency=0x4E2E7700C8f8A414b4AF8828f54330a304B6625d"> Free Coin, </a>
               </span>
               and
-                <span className="animate-gradient">
+              <span className="animate-gradient">
                 <a href="https://swap.crodex.app/#/swap?outputCurrency=0x3E68eA1588E32e51D98dBda6Ea05128513ECC713"> BrokeASF </a>
               </span>
-For only .03 percent!
-
+              For only .03 percent!
             </h1>
 
             <p className="dark:text-jacarta-200 mb-8 text-center text-lg md:text-left">
              
             </p>
            
-<div className="flex space-x-4">
-  <Link href="https://swap.crodex.app/#/swap?outputCurrency=0x4E2E7700C8f8A414b4AF8828f54330a304B6625d">
-    <a className="text-white w-fit-content">
-      <img src="images/freeCoinTokenSwap.png" alt="Free Coin Image" width="75%" height="75%" />
-      <p className="font-semibold hover:text-white">Free Coin 🪙</p>
-    </a>
-  </Link>
-  <Link href="https://swap.crodex.app/#/swap?outputCurrency=0xe7f3c7c6611357313b5c2734341fe9cbad1d9f59">
-    <a className="text-white">
-      <img src="/images/sycoticTokenSwap.png" alt="Sycotic Society Image" width="75%" height="75%" />
-      <p className="font-semibold hover:text-white">💢Sycotic💢</p>
-    </a>
-  </Link>
-  <Link href="https://swap.crodex.app/#/swap?outputCurrency=0x3E68eA1588E32e51D98dBda6Ea05128513ECC713">
-    <a className="text-white">
-      <img src="/images/brokeAsfTokenSwap.png" alt="Broke ASF Image" width="75%" height="75%" />
-      <p className="font-semibold hover:text-white">Broke ASF🐒</p>
-    </a>
-  </Link>
-</div>
-
-
-
-
+            <div className="flex space-x-4">
+              <Link href="https://swap.crodex.app/#/swap?outputCurrency=0x4E2E7700C8f8A414b4AF8828f54330a304B6625d">
+                <a className="text-white w-fit-content">
+                  <img src="images/freeCoinTokenSwap.png" alt="Free Coin Image" width="75%" height="75%" />
+                  <p className="font-semibold hover:text-white">Free Coin 🪙</p>
+                </a>
+              </Link>
+              <Link href="https://swap.crodex.app/#/swap?outputCurrency=0xe7f3c7c6611357313b5c2734341fe9cbad1d9f59">
+                <a className="text-white">
+                  <img src="/images/sycoticTokenSwap.png" alt="Sycotic Society Image" width="75%" height="75%" />
+                  <p className="font-semibold hover:text-white">💢Sycotic💢</p>
+                </a>
+              </Link>
+              <Link href="https://swap.crodex.app/#/swap?outputCurrency=0x3E68eA1588E32e51D98dBda6Ea05128513ECC713">
+                <a className="text-white">
+                  <img src="/images/brokeAsfTokenSwap.png" alt="Broke ASF Image" width="75%" height="75%" />
+                  <p className="font-semibold hover:text-white">Broke ASF🐒</p>
+                </a>
+              </Link>
+            </div>
           </div>
 
           {/* Hero images */}
-         <div className="col-span-6 xl:col-span-8 relative">
-  <div className="text-center md:pl-8 md:text-right">
-    {/* Base image */}
-    <img
-      src="/images/hero/hero.jpg"
-      alt=""
-      className="hero-img mt-8 inline-block w-72 rotate-[8deg] sm:w-full lg:w-[24rem] lg:w-[24rem]"
-      onClick={handleImageClick}
-      style={{ zIndex: 1 }} // Set a higher z-index for the base image
-    />
-    {/* 3D Hero element */}
-   
-  </div>
-</div>
-
+          <div className="col-span-6 xl:col-span-8 relative">
+            <div className="text-center md:pl-8 md:text-right">
+              {/* Base image */}
+              <img
+                src="/images/hero/hero.jpg"
+                alt=""
+                className="hero-img mt-8 inline-block w-72 rotate-[8deg] sm:w-full lg:w-[24rem]"
+                onClick={handleImageClick}
+                style={{ zIndex: 1 }} // Set a higher z-index for the base image
+              />
+              {/* 3D Hero element */}
+            </div>
+          </div>
         </div>
       </div>
     </section>
